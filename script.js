3503e2d341845340b4bb822a8851a865a042bb2a
Amanda <awiehrs.photo@gmail.com>
2021-01-24 00:00:44 -0500

    added styling for single page portfolio, added images to use, implemented jquery animations for transitions

---
+++ script.js
@@ -0,0 +1,73 @@
+$(document).ready(function() {
+    var aboutLink = $(".aboutBtn")
+    var photoLink = $(".photoBtn")
+    var codeLink = $(".codeBtn")
+    var nameLink = $(".nameHead")
+    var contactLink = $(".contactBtn")
+
+    function aboutInit(e) {
+        e.preventDefault();
+        $(".frontImg").slideUp(1050);
+        $(".aboutDiv").animate({width: '100%', opacity: '0.8'}, 2000);
+        $(".photoDiv").hide(300);
+        $(".codeDiv").hide(300);
+        $(".contactContent").hide();
+        $(".aboutContent").fadeIn(4000);
+        $(".aboutDiv").addClass("boxWidth");
+        $(".aboutDiv").show();
+    };
+    
+    function photoInit(e) {
+        e.preventDefault();
+        $(".frontImg").slideUp(1050);
+        $(".photoDiv").animate({width: '100%', opacity: '0.8'}, 2000);
+        $(".aboutDiv").hide(300);
+        $(".codeDiv").hide(300);
+        $(".contactContent").hide();
+        $(".photoContent").fadeIn(4000);
+        $(".photoDiv").addClass("boxWidth");
+        $(".photoDiv").show();
+        $(".photoDiv").removeClass("text-center");
+        $(".photoTxt").removeClass("titlePosition");
+    };
+
+    function codeInit(e) {
+        e.preventDefault();
+        $(".frontImg").slideUp(1050);
+        $(".codeDiv").animate({width: '100%', opacity: '0.8'}, 2000);
+        $(".photoDiv").hide(300);
+        $(".aboutDiv").hide(300);
+        $(".contactContent").hide();
+        $(".codeContent").fadeIn(4000);
+        $(".codeDiv").addClass("boxWidth");
+        $(".codeDiv").show();
+        $(".codeDiv").removeClass("text-center");
+        $(".codeTxt").removeClass("titlePosition");
+    };
+    
+    function resetLanding(e) {
+        e.preventDefault();
+        location.reload();
+    };
+
+    function contactInit(e) {
+        e.preventDefault();
+        $(".frontImg").slideUp(1050);
+        $(".photoDiv").hide(300);
+        $(".aboutDiv").hide(300);
+        $(".codeDiv").hide(300);
+        $(".contactContent").show(2000);
+
+    }
+  
+    $(aboutLink).click(aboutInit);
+    $(photoLink).click(photoInit);
+    $(codeLink).click(codeInit);
+    $(nameLink).click(resetLanding);
+    $(contactLink).click(contactInit);
+
+
+
+
+
+});
